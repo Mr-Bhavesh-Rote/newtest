9722545fc23f1280d19e7088838c4ef3d3478582
Mr-Bhavesh-Rote <122857430+Mr-Bhavesh-Rote@users.noreply.github.com>
2023-08-30 23:03:44 +0530

    Update app_main.py

--- app_main.py
+++ app_main.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 @app.route('/hello', methods=['GET'])
 def hello():
     return "Hello, World!"
+import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
     app.run()
